24bde737b8e6993a9791fd54ce8540fd7cc06cc7
Raj Tejaswee <rajtejaswee02@gmail.com>
2025-11-15 05:36:53 +0530

    cors

---
+++ backend/cors/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.post('/sum', (req, res) => {
+    const a = parseInt(req.body.a)
+    const b = parseInt(req.body.b)
+    const sum = a + b
+
+    res.send({ sum })
+})
+
+app.listen(3000)
