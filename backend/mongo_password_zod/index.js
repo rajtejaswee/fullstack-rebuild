99ee9f5784c39c13338a01e7eae5c0e2f22308a8
Raj Tejaswee <rajtejaswee02@gmail.com>
2025-11-25 17:36:00 +0530

    complete backend todoapp

--- backend/mongo_password_zod/index.js
+++ backend/mongo_password_zod/index.js
@@ -9,7 +9,7 @@ const  {signupSchema, loginSchema,todoSchema } = require('./validation')
 const app = express();
 
 app.use(express.json())
-mongoose.connect("mongodb+srv://rajtejaswee02_db_user:kRm7MUYpbkWvfZr1@cluster0.oafpmuz.mongodb.net/?appName=Cluster0")
+mongoose.connect("")
 
 app.post("/signup", async function (req, res) {
     const parse = signupSchema.safeParse(req.body)
@@ -18,7 +18,7 @@ app.post("/signup", async function (req, res) {
     }
     const {name, email, password} = parse.data
 
-    const hashPassword = await bcrpyt.hash(password, 10);
+    const hashPassword = await bcrypt.hash(password, 10);
     
     await UserModel.create({
         email: email,
@@ -35,42 +35,29 @@ app.post("/login", async function (req, res) {
 
     const parse = loginSchema.safeParse(req.body)
     if (!parse.success) {
-        return res.status(403).json(parse.error)
+        return res.status(400).json(parse.error)
     }
     const {email, password} = parse.data
-
     const user = await UserModel.findOne({
         email:email
     })
     if (!user) return res.status(403).json({ message: "Invalid Credentials" })
     const checkPassword = await bcrypt.compare(password, user.password);
     if(!checkPassword) return res.status(403).json({ message: "Invalid Credentials" })
-    if (user && checkPassword) {
-        const token = jwt.sign({
-            id:user._id.toString()
-        }, JWT_SECRET)
-        res.send({
-            token
-        })
-    }
-    else {
-        res.status(403).json({
-            message:"Invalid Credentials"
-        })
-    }
+    const token = jwt.sign({ id: user._id.toString() }, JWT_SECRET)
+    return res.json({ token })
 
 })
 
 app.post("/todo", auth, async function (req, res) {
-    const { title } = req.body;
-    if (!title) {
-        res.send({
-            message: "Title is missing"
-        })
+    const parse = todoSchema.safeParse(req.body)
+    if (!parse.success) {
+        return res.status(400).json(parse.error)
     }
-    const todo = TodoModel.create({
+    const { title } = parse.data
+    const todo = await TodoModel.create({
         userId: req.user.id,
-        title: title,
+        title,
         done: false
     })
     res.json(todo)
@@ -81,6 +68,57 @@ app.get("/todos", auth, async function (req, res) {
         userId: req.user.id
     })
     res.send(todos)
+})
+
+app.put("/todo/:id", auth, async function (req, res) {
+    const parse = todoSchema.safeParse(req.body)
+    if (!parse.success) return res.status(400).json(parse.error)
+
+    const { title } = parse.data
+    const { id } = req.params
+
+    const todo = await TodoModel.findById(id);
+    if (!todo) {
+       return res.status(404).send("Todo not found")
+    }
+    if (todo.userId.toString() !== req.user.id) {
+        return res.status(403).send("Unauthorized")
+    }
+    todo.title = title
+    await todo.save()
+
+    res.json(todo)
+})
+
+app.put("/todo/:id/done", auth, async function (req, res) {
+    const { id } = req.params
+    
+    const todo = await TodoModel.findById(id)
+    if (!todo) {
+        return res.status(404).send("Todo not found")
+    }
+    if (todo.userId.toString() !== req.user.id) {
+        return res.status(403).send("Unauthorized")
+    }
+    todo.done = true
+    await todo.save()
+    res.send(todo)
+})
+
+app.delete("/todo/:id", auth, async function (req, res) {
+    const { id } = req.params;
+    const todo = await TodoModel.findById(id)
+    if (!todo) {
+        return res.status(404).send("Todo not found")
+    }
+    if (todo.userId.toString() !== req.user.id) {
+        return res.status(403).send("Unauthorized")
+    }
+    await TodoModel.deleteOne({ _id: id })
+    res.json({
+        message: "Todo deleted"
+    })
+
 })
 
 app.listen(3000);
--- backend/mongo_password_zod/validation.js
+++ backend/mongo_password_zod/validation.js
@@ -2,12 +2,12 @@ const { z } = require('zod');
 
 const signupSchema = z.object({
     name: z.string().min(1),
-    email: z.string().email,
+    email: z.string().email(),
     password: z.string().min(1)
 })
 
 const loginSchema = z.object({
-    email: z.string().email,
+    email: z.string().email(),
     password: z.string().min(1)
 })
 
