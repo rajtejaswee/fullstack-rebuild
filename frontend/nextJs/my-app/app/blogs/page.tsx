9d9694781ef6cb48eb8757e71170d66640dfa067
Raj Tejaswee <rajtejaswee02@gmail.com>
2025-12-09 18:33:48 +0530

    day 1

---
+++ frontend/nextJs/my-app/app/blogs/page.tsx
@@ -0,0 +1,26 @@
+import axios from "axios"
+
+async function getBlogs() {
+    const response = await axios.get("https://jsonplaceholder.typicode.com/todos/")
+    return response.data;
+}
+
+export default async function Blogs() {
+
+    const blogs = await getBlogs();
+    return <div>
+        {blogs.map((blog: ITodo) => <Todo title={blog.title} completed={blog.completed} />)}
+    </div>
+}
+
+
+interface ITodo {
+    completed: boolean;
+    title: string;
+}
+
+function Todo({title, completed}:ITodo) {
+    return <div>
+        {title} {completed? "done":"not done"}
+    </div>
+}
